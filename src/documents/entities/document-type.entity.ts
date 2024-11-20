294458ee8bc3b9c31490d9081014fd4b728b075d
Emanuelvss13 <emanuelsouza2002@gmail.com>
2024-11-20 20:55:10 -0300

    refactor: add totalTaxes and netValue in document entity

--- src/documents/entities/document-type.entity.ts
+++ src/documents/entities/document-type.entity.ts
@@ -1,8 +1,8 @@
 export class DocumentType {
-  private readonly id: string;
+  private readonly id: number;
   private readonly name: string;
 
-  constructor(id: string, name: string) {
+  constructor(id: number, name: string) {
     this.id = id;
     this.name = name;
   }
--- src/documents/entities/document.entity.ts
+++ src/documents/entities/document.entity.ts
@@ -1,10 +1,13 @@
 import { DocumentOrigin } from '../enums/document-origin.enum';
+import { DocumentType } from './document-type.entity';
 
 export class Document {
   private readonly id: string;
   private readonly title: string;
   private readonly code: string;
   private readonly emitter: string;
+  private readonly totalTaxes: number;
+  private readonly netValue: number;
   private readonly type: DocumentType;
   private readonly origin: DocumentOrigin;
   private readonly createdAt: Date;
@@ -15,6 +18,8 @@ export class Document {
     code: string,
     title: string,
     emitter: string,
+    totalTaxes: number,
+    netValue: number,
     type: DocumentType,
     origin: DocumentOrigin,
     createdAt: Date,
@@ -24,6 +29,8 @@ export class Document {
     this.title = title;
     this.code = code;
     this.emitter = emitter;
+    this.totalTaxes = totalTaxes;
+    this.netValue = netValue;
     this.type = type;
     this.origin = origin;
     this.createdAt = createdAt;
