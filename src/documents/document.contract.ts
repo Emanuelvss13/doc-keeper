53d47bc6cc3cae3fb08e665c87fe46870da06877
Emanuelvss13 <emanuelsouza2002@gmail.com>
2024-11-21 22:06:03 -0300

    feat: create document contract

--- src/documents/document.contract.ts
+++ src/documents/document.contract.ts
@@ -1,5 +1,6 @@
 import { HttpStatus } from '@nestjs/common';
 import { initContract } from '@ts-rest/core';
+import { ICreateDocumentDTO } from './dto/create-document.dto';
 import { Document } from './entities/document.entity';
 
 const c = initContract();
@@ -21,5 +22,18 @@ export const DocumentControllerContract = c.router({
       404: c.type<{ code: HttpStatus; error: string }>(),
     },
     summary: 'Get a document by id',
+  },
+  createDocument: {
+    method: 'POST',
+    path: '/documents',
+    contentType: 'multipart/form-data',
+    body: c.type<{
+      file: Blob;
+      data: ICreateDocumentDTO;
+    }>(),
+    responses: {
+      200: c.type<Document>(),
+    },
+    summary: 'Create a new document',
   },
 });
